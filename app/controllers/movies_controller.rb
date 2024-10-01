ea8f4b05186436023363f5405feccbce0c6d5e86
manojpeta-gh <manoj.peta@tamu.edu>
2024-10-01 02:11:27 -0500

    all changes done

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,7 +4,14 @@ class MoviesController < ApplicationController
   # GET /movies or /movies.json
   def index
     @movies = Movie.all
+    # Determine sorting column and direction, default is by title, ascending
+    sort_column = params[:sort_by] || 'title'
+    sort_direction = params[:direction] == 'desc' ? 'desc' : 'asc'
+
+    # Order the movies by the chosen column and direction
+    @movies = Movie.order("#{sort_column} #{sort_direction}")
   end
+
 
   # GET /movies/1 or /movies/1.json
   def show
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,11 @@
 module ApplicationHelper
+    def index
+        @movies = Movie.all
+        # Determine sorting column and direction, default is by title, ascending
+        sort_column = params[:sort_by] || 'title'
+        sort_direction = params[:direction] == 'desc' ? 'desc' : 'asc'
     
+        # Order the movies by the chosen column and direction
+        @movies = Movie.order("#{sort_column} #{sort_direction}")
+      end    
 end
